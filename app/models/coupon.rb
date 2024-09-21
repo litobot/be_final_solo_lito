49b398a261d9afc2d7e52af5d33644afd1b3c9f0
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-09-21 15:03:44 -0600

    Add Coupon model & all model validations

---
+++ app/models/coupon.rb
@@ -0,0 +1,9 @@
+class Coupon < ApplicationRecord
+  belongs_to :merchant
+  has_many :invoices, optional: true
+
+  validates :code, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :percent_off, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 100 }
+  validates :status, presence: true, inclusion: { in: ["active", "inactive"] }
+end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,5 +5,8 @@ class Invoice < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :transactions, dependent: :destroy
 
+  validates :coupon_id, presence: true
+  validates :customer_id, presence: true
+  validates :merchant_id, presence: true
   validates :status, inclusion: { in: ["shipped", "packaged", "returned"] }
 end
