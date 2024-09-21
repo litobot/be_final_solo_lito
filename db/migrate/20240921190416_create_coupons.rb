6adf158221a50d8a61dc64655b4440018c6ed719
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-09-21 13:26:57 -0600

    Initial Commit

---
+++ db/migrate/20240921190416_create_coupons.rb
@@ -0,0 +1,12 @@
+class CreateCoupons < ActiveRecord::Migration[7.1]
+  def change
+    create_table :coupons do |t|
+      t.string :code
+      t.decimal :percent_off
+      t.string :status
+      t.references :merchant, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
