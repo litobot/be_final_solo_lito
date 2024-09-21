954fd33ce23f34d1f09cd1fc3d7353936deeddd9
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-09-21 14:44:40 -0600

    Add Coupon Table & needed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_21_191807) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_21_204214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "coupons", force: :cascade do |t|
     t.string "code"
+    t.string "name"
     t.decimal "percent_off"
     t.string "status"
     t.bigint "merchant_id", null: false
